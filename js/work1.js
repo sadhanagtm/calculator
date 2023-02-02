1e3331abf54490ce09bfa13c4756da1eca9f2dfa
sadhanagtm <sadhanagaut@gmail.com>
2023-02-02 12:02:49 +0545

    calculatordone

--- js/work1.js
+++ js/work1.js
@@ -9,12 +9,12 @@ function Numb(a){
     }
     else{
         ending+=a;
-        show.value+=ending
+        show.value+=a
     }
     console.log(starting,opt,ending)
 } 
 function Operator(a){
-    opt=a;
+    opt+=a;
     show.value+=a;
 }
 function Result(){
@@ -39,4 +39,28 @@ function Clear(){
     ending="";
     opt="";
     show.value="0";
+}
+function Partial(){
+    if(opt.length===0){
+        let lgt=starting.length;
+        let data=starting.slice(0,lgt-1);
+        console.log(starting,data);
+        starting=data;
+        show.value=data;
+    }
+    else if(opt.length>=1 && ending.length===0){
+        let lgt=opt.length;
+        console.log(lgt)
+        let data=opt.slice(0,lgt-1);
+        console.log(opt,data);
+        opt=data;
+        show.value= starting+data
+    }
+    else{
+        let lgt=ending.length;
+        let data=ending.slice(0,lgt-1);
+        console.log(ending,data,opt);
+        ending=data;
+        show.value=starting+opt+ending;
+    }
 }
